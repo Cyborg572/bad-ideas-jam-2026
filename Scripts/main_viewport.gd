51a3e4ed50b63ff431f3ed42920cd110325057ac
Mike Gossmann <mike@c572.ca>
2026-03-12 16:16:29 -0400

    Better viewport resizing and gym improvements

---
+++ Scripts/main_viewport.gd
@@ -0,0 +1,13 @@
+extends SubViewportContainer
+
+@onready var sub_viewport: SubViewport = $SubViewport
+
+func resize_3D_display() -> void:
+	print("Resizing!")
+	var window_size := get_viewport().get_visible_rect().size
+	sub_viewport.size = window_size
+
+func _ready() -> void:
+	resize_3D_display()
+	get_viewport().size_changed.connect(resize_3D_display)
+	
