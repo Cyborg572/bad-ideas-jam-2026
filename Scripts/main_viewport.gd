3fcddbffab1356ac5f60983b3cfa6b7f6d39c309
Mike Gossmann <mike@c572.ca>
2026-03-20 14:34:08 -0400

    Use automatic subviewport resizing

--- Scripts/main_viewport.gd
+++ Scripts/main_viewport.gd
@@ -2,10 +2,10 @@ extends SubViewportContainer
 
 @onready var sub_viewport: SubViewport = $SubViewport
 
-func resize_3D_display() -> void:
-	var window_size := get_viewport().get_visible_rect().size
-	sub_viewport.size = window_size
-
-func _ready() -> void:
-	resize_3D_display()
-	get_viewport().size_changed.connect(resize_3D_display)
+#func resize_3D_display() -> void:
+	#var window_size := get_viewport().get_visible_rect().size
+	#sub_viewport.size = window_size
+#
+#func _ready() -> void:
+	#resize_3D_display()
+	#get_viewport().size_changed.connect(resize_3D_display)
